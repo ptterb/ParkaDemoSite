81e84722e734d2b048863d984cc0eab33ddf010b
ptterb <bjppolo@gmail.com>
2019-04-17 17:27:43 -0400

    Add 3d viewer, scripts for interaction

---
+++ _site/js/interaction.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $( "#explore" ).click(function() {
+    $( "#explore" ).toggle();
+    $( "#semi-transparent" ).toggle();
+    $( "#learn-more" ).toggle();
+    $( ".header-content-inner" ).toggle();
+   
+
+  });
+});
+
